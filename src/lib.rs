75f6388a11e861673f8491be2e25e9051832a38c
Jesus Jayaro <jayaro@gmail.com>
2022-05-30 21:46:52 -0600

    Chapter 3.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
 use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
-
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result <Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result <Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-    .bind(("127.0.0.1", 8000))?
+    .listen(listener)?//.bind(address)?
     .run();
     Ok(server)
 }
